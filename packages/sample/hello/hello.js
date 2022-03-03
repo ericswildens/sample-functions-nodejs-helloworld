f7cafd77336231fcda20dfca23a8ccbd49ced5cb
Eric Swildens <eric_x@waba.com>
2022-03-02 21:15:25 -0800

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,5 +1,5 @@
 function main(args) {
-    let name = args.name || 'stranger'
+    let name = args.name || 'from_app_platform'
     let greeting = 'Hello ' + name + '!'
     console.log(greeting)
     return {"body": greeting}
